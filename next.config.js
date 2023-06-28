e2b3a94b274cd63d41040654329af5592db45cec
Alex <alexthetaffer@gmail.com>
2023-06-28 16:03:33 +0300

    Deployment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
     basePath: '/personal-website',
+    exportPathMap: function () {
+        return {
+          '/': { page: '/' },
+        };
+      },
+      trailingSlash: true,
 };
 
 module.exports = nextConfig;
