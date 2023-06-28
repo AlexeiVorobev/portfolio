2827ff739af5fa926dd4fb84c74d461defdd55b5
Alex <alexthetaffer@gmail.com>
2023-06-28 11:51:04 +0300

    Fix skills image links

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = () => {
             <div key={skill.title} className="p-6 shadow-md rounded-xl hover:scale-105 ease-in duration-300">
               <div className="grid grid-cols-2 gap-4 justify-center items-center">
                 <div className="m-auto">
-                  <Image src={"/../public/assets/skills/" + skill.img} width="64" height="64" alt="/" />
+                  <Image src={"/assets/skills/" + skill.img} width="64" height="64" alt="/" />
                 </div>
                 <div className="flex flex-col items-center justify-center">
                   <h3>{skill.title}</h3>
