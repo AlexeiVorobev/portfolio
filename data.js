89f61eee834955e845dc473efcb62956d37471a9
Alex <alexthetaffer@gmail.com>
2023-06-28 12:08:13 +0300

    Updates

---
+++ data.js
@@ -0,0 +1,36 @@
+
+export const skills = [
+    {
+        title: 'CSS',
+        img: "css.png"
+    },
+    {
+        title: 'JavaScript',
+        img: "javascript.png"
+    },
+    {
+        title: 'React',
+        img: "react.png"
+    },
+    {
+        title: 'NodeJS',
+        img: "node.png"
+    },
+    {
+        title: 'MongoDB',
+        img: "mongo.png"
+    },
+    {
+        title: 'NextJS',
+        img: "nextjs.png"
+    },
+    {
+        title: 'Tailwind',
+        img: "tailwind.png"
+    },
+    {
+        title: 'Github',
+        img: "github1.png"
+    },
+    
+]
