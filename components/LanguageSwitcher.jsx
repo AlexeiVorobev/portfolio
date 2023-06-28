eb944b0fe04fd9b921d7a0cff01ba4370cf5b01f
Alex <alexthetaffer@gmail.com>
2023-06-28 11:30:42 +0300

    Fix local storage issue

--- components/LanguageSwitcher.jsx
+++ components/LanguageSwitcher.jsx
@@ -1,16 +1,23 @@
 "use client"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const LanguageSwitcher = ({color}) => {
   const { t, i18n } = useTranslation();
   const handleChangeLanguage = (lang) => {
     i18n.changeLanguage(lang);
+    setStoredLanguage(lang)
     localStorage.setItem("i18nextLng", lang);
   };
 
-   // Get the stored language from local storage
-   const storedLanguage = localStorage.getItem("i18nextLng").substring(0,2) || 'ru';
+  const [storedLanguage, setStoredLanguage] = useState(null)
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setStoredLanguage(localStorage.getItem("i18nextLng").substring(0,2) || 'ru')
+    }
+  }, [])
+
   return (
     <div>
       <select
