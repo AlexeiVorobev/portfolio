3f16dfbff0145b0853de1b1efea3efdaabb808d3
Alex <alexthetaffer@gmail.com>
2023-06-28 12:15:44 +0300

    Update nextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    basePath: '/personal-website',
+    assetPrefix: '/personal-website/',
 };
 
 module.exports = nextConfig;
