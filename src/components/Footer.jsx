43543b315efed8700fb4bb1125c7af2c50a9530d
Alex <alexthetaffer@gmail.com>
2023-07-03 09:31:39 +0300

    Style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,42 @@
-import React from "react";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiFillGithub,
+  AiFillMail,
+  AiFillLinkedin,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 py-8">
+    <footer className="bg-gray-800 py-8">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-col md:flex-row items-center text-center md:text-left justify-between">
           <div className="text-white">
-            <h4 className="text-lg font-bold">Your Name</h4>
+            <h4 className="text-lg font-bold">Alexei Vorobev</h4>
             <p className="text-sm">Full-Stack Web Developer</p>
           </div>
-          <div className="flex mt-4 md:mt-0">
-            <a
-              href="https://github.com/yourusername"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white hover:text-gray-400 ml-4"
-            >
-              GitHub
+          <div className="flex items-center justify-center md:justify-end my-4 w-full sm:w-[80%]">
+            <a href="https://github.com/AlexeiVorobev" target="_blank">
+              <div className="rounded-full text-xl text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                <AiFillGithub />
+              </div>
             </a>
-            <a
-              href="https://linkedin.com/in/yourusername"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white hover:text-gray-400 ml-4"
-            >
-              LinkedIn
-            </a>
-            <a
-              href="mailto:youremail@example.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white hover:text-gray-400 ml-4"
-            >
-              Email
-            </a>
+            <div className="rounded-full text-xl text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <AiFillLinkedin />
+            </div>
+            <div className="rounded-full text-xl text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <AiFillMail />
+            </div>
           </div>
         </div>
-        <div className="mt-4 border-t border-gray-700 pt-4 text-sm text-gray-400">
-          <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+        <div className="mt-4 border-t border-gray-700 pt-4 text-sm text-gray-400 text-center md:text-left">
+          <p>
+            &copy; {new Date().getFullYear()} Alexei Vorobev. All rights
+            reserved.
+          </p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
