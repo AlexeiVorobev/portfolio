cdf3896a36196f6d9c0414d1b4f08eb86aec9808
Alex <alexthetaffer@gmail.com>
2023-07-03 14:41:15 +0300

    Translation and content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,20 +11,20 @@ const Footer = () => {
     <footer className="bg-gray-800 py-8">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row items-center text-center md:text-left justify-between">
-          <div className="text-white">
+          <div className="text-gray-100">
             <h4 className="text-lg font-bold">Alexei Vorobev</h4>
             <p className="text-sm">Full-Stack Web Developer</p>
           </div>
           <div className="flex items-center justify-center md:justify-end my-4 w-full sm:w-[80%]">
             <a href="https://github.com/AlexeiVorobev" target="_blank">
-              <div className="rounded-full text-xl text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+              <div className="rounded-full text-xl text-gray-200 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
                 <AiFillGithub />
               </div>
             </a>
-            <div className="rounded-full text-xl text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+            <div className="rounded-full text-xl text-gray-200 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
               <AiFillLinkedin />
             </div>
-            <div className="rounded-full text-xl text-white p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+            <div className="rounded-full text-xl text-gray-200 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
               <AiFillMail />
             </div>
           </div>
