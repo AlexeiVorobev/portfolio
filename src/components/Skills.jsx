2a8aee9726769ebd56d0bd3e13666d8176af3b92
Alex <alexthetaffer@gmail.com>
2023-07-03 15:18:26 +0300

    Style Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SkillItem from "./SkillItem"
+import { useTranslation } from "react-i18next";
 
 import cssImg from '../assets/skills/css.png'
 import javaScriptImg from '../assets/skills/javascript.png'
@@ -8,24 +9,26 @@ import nodejsImg from '../assets/skills/node.png'
 import mongoImg from '../assets/skills/mongo.png'
 import tailwindImg from '../assets/skills/tailwind.png'
 import gitHubImg from '../assets/skills/github1.png'
+import typeScriptImg from '../assets/skills/typescript.png'
 
 const Skills = () => {
+  const { t } = useTranslation();
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
-          Skills
+          {t("navbar.skills")}
         </p>
-        <h2 className="py-4">What I Can Do</h2>
+        <h2 className="py-4">{t("skills.h")}</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
            <SkillItem title="CSS" img={cssImg} />
            <SkillItem title="JavaScript" img={javaScriptImg} />
            <SkillItem title="React" img={reactImg} />
+           <SkillItem title="TypeScript" img={typeScriptImg} />
            <SkillItem title="NodeJS" img={nodejsImg} />
            <SkillItem title="MongoDB" img={mongoImg} />
            <SkillItem title="Tailwind CSS" img={tailwindImg} />
            <SkillItem title="Github" img={gitHubImg} />
-           <SkillItem title="CSS" img={cssImg} />
         </div>
       </div>
     </div>
